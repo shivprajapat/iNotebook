42796fa79b3906c49bf8276569f9c4a086916d9e
shivkumarprajapat <shivkumarprajapat232@gmail.com>
2022-01-02 11:14:17 +0530

    connet MongoDB functionality

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("Hello React");
+const connectToMongo = require("./db");
+const express = require("express");
+
+connectToMongo();
+app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello React");
+});
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening at http://localhost:${port}`);
+});
