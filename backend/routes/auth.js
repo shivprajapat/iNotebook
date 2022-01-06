bb2ab064c96892fd220efd0c4b6be757cae525fe
shivkumarprajapat <shivkumarprajapat232@gmail.com>
2022-01-06 20:57:50 +0530

    middleware to decode user from a JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
-
+var fetchuser = require("../middleware/fetchuser");
 const JWT_SECRET = "Harryisagoodb$oy";
 
 // Create a User using: POST "/api/auth/createuser". No login required
@@ -99,4 +99,15 @@ router.post(
     }
   }
 );
+// ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = router;
